1a0fbee2d6d6ac4ba44384bc366754cfbda3bce0
Miguel Serrano <sermi@MartinaPC.(none)>
2024-07-23 12:10:20 -0400

    Actualizare con lo que hizo el profe

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const togglebuton = document.querySelector("#toggle-theme");
+togglebuton.addEventListener("click", function (){
+    const htmlTag = document.querySelector("html");
+    const currentValue = htmlTag.getAttribute
+}
